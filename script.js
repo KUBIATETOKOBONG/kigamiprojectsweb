02f4bbe0aa63cb1332f4c6091fca7b64887b3c1c
KUBIAT ETOKOBONG <kubiatetokobong@gmail.com>
2026-05-20 18:17:21 +0100

    Update hamburger menu and site improvements

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
-ffunction toggleMenu() {
-  const menu = document.getElementById('mobileMenu');
+function toggleMenu() {
+  const mobileMenu = document.getElementById("mobileMenu");
 
-  menu.classList.toggle('show');
-}
-
-  const expanded =
-    hamburger.getAttribute('aria-expanded') === 'true';
-
-  if (expanded) {
-    hamburger.setAttribute('aria-expanded', 'false');
-    navList.classList.remove('active');
-  } else {
-    hamburger.setAttribute('aria-expanded', 'true');
-    navList.classList.add('active');
-  }
+  mobileMenu.classList.toggle("show");
 }
